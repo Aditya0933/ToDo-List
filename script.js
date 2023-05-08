597c6873b17792ecc211990a75ef3e9c46252097
Aditya Singh Parihar <201037@kit.ac.in>
2023-05-08 17:35:32 +0530

    Done

--- script.js
+++ script.js
@@ -11,15 +11,30 @@ document.getElementById('todo-form').addEventListener('submit', function(e) {
   
     var listItem = document.createElement('li');
     listItem.innerText = todoInput.value;
+    const local_val = todoInput.value;
+    // console.log(local_val)
+    // console.log(typeof(local_val))
+    // local_val.charAt(0).style.textTransform = "capitalize"
+    // local_val[0].style.height = "50"+px
     todoList.appendChild(listItem);
      // check Box implementtatino
     var x = document.createElement("input");
     x.setAttribute("type", "checkbox");
-    todoList.appendChild(x);
+    x.setAttribute('class','checkbox')
+    listItem.appendChild(x);
     
+    // localStorage.setItem(8765, todoInput.value);
+  // console.log(local_val);
+  // while(local_val === true){
+  //   console.log("HOOOOOOOOOOOO")
+  //   let i = 0
+  //   localStorage.setItem(i, local_val);
+  //   i++
+  //   break
+  // }
 
-    
-  
+ 
+
     todoInput.value = ''; // Clear input field
   });
   
